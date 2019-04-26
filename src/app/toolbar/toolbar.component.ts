96384cacd66e6675e87a7793b0fbb21928c1d205
Tom Wolfe <trwolfe13@gmail.com>
2019-04-26 14:15:50 +0100

    Add file input control

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,16 +1,34 @@
-import { Component, HostListener, ElementRef, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+
+interface ToolbarItem {
+  title: string;
+  icon: string;
+  execute: () => void;
+}
 
 @Component({
   selector: 'bm-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements AfterViewInit {
+  toolbarItems: ToolbarItem[] = [
+    { title: 'Grid', icon: 'mdi-grid', execute: () => { } },
+    { title: 'Background Image', icon: 'mdi-image', execute: this.onBackgroundImageClicked },
+    { title: 'Tokens', icon: 'mdi-chess-knight', execute: () => { } },
+    { title: 'Move', icon: 'mdi-cursor-move', execute: () => { } },
+    { title: 'Painting', icon: 'mdi-brush', execute: () => { } },
+    { title: 'Conditions', icon: 'mdi-flask', execute: () => { } },
+    { title: 'Distance', icon: 'mdi-ruler', execute: () => { } },
+  ];
+
+  @ViewChild('file') inputRef: ElementRef<HTMLInputElement>;
 
   constructor(private elRef: ElementRef) { }
 
-  ngOnInit() { this.reposition(); }
   @HostListener('window:resize') onResize() { this.reposition(); }
+
+  ngAfterViewInit(): void { this.reposition(); }
 
   reposition() {
     const el = this.elRef.nativeElement as HTMLElement;
@@ -18,5 +36,19 @@ export class ToolbarComponent implements OnInit {
     const midPoint = el.clientHeight / 2;
     const top = halfBody - midPoint;
     el.style.top = `${top}px`;
+  }
+
+  onBackgroundImageClicked() {
+    this.inputRef.nativeElement.click();
+  }
+
+  onBackgroundImageChange() {
+    const image = this.inputRef.nativeElement.files[0];
+    const url = URL.createObjectURL(image);
+    console.log(url);
+  }
+
+  onToolbarItemClick(item: ToolbarItem) {
+    item.execute.bind(this)();
   }
 }
