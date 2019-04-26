2fd07fba0590c9bf26b8338af1708d7a96470c31
Tom Wolfe <trwolfe13@gmail.com>
2019-04-26 13:28:12 +0100

    Allow panning

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -21,35 +21,59 @@ export class MapComponent implements OnInit, OnChanges {
   ngOnInit() {
     this.canvas = this.canvasEl.nativeElement;
     this.context = this.canvas.getContext('2d');
+    this.onResize();
+  }
+
+  ngOnChanges() { this.onResize(); }
+
+  @HostListener('window:resize') onResize() {
+    this.canvas.width = this.elRef.nativeElement.clientWidth;
+    this.canvas.height = this.elRef.nativeElement.clientHeight;
     this.render();
   }
 
-  ngOnChanges() { this.render(); }
-  @HostListener('window:resize') onResize() { this.render(); }
+  panOffset = { x: 0, y: 0 };
+
+  @HostListener('panmove', ['$event']) onPanMove(e: any) {
+    this.panOffset = {
+      x: e.deltaX,
+      y: e.deltaY
+    };
+    this.render();
+  }
+
+  @HostListener('panend', ['$event']) onPanEnd(e: any) {
+    this.gridOffset.x += this.panOffset.x;
+    this.gridOffset.y += this.panOffset.y;
+  }
 
   render() {
-    this.updateSize();
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.renderGrid();
   }
 
-  updateSize() {
-    this.canvas.width = this.elRef.nativeElement.clientWidth;
-    this.canvas.height = this.elRef.nativeElement.clientHeight;
-  }
-
   renderGrid() {
-    this.context.lineWidth = 1;
     this.context.strokeStyle = 'rgba(255, 255, 255, 0.1)';
+
+    const startX = this.boundCoordinate(this.gridOffset.x + this.panOffset.x + this.gridSize);
+    const startY = this.boundCoordinate(this.gridOffset.y + this.panOffset.y + this.gridSize);
+
     const grid = new Path2D();
-    for (let x = this.gridOffset.x + this.gridSize; x <= this.canvas.width; x += this.gridSize) {
+    for (let x = startX; x <= this.canvas.width; x += this.gridSize) {
       grid.moveTo(x, 0);
       grid.lineTo(x, this.canvas.height);
     }
-
-    for (let y = this.gridOffset.y + this.gridSize; y <= this.canvas.height; y += this.gridSize) {
+    for (let y = startY; y <= this.canvas.height; y += this.gridSize) {
       grid.moveTo(0, y);
       grid.lineTo(this.canvas.width, y);
     }
     this.context.stroke(grid);
+  }
+
+  private boundCoordinate(ord: number) {
+    if (ord > this.gridSize || ord < 0) {
+      ord -= Math.ceil(ord / this.gridSize) * this.gridSize;
+    }
+    return ord
   }
 }
