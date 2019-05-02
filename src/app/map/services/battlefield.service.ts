8ebd00cfa3e6880f3ad029844d24ee86349a87ca
Tom Wolfe <trwolfe13@gmail.com>
2019-05-02 19:47:59 +0100

    Add todo note

--- src/app/map/services/battlefield.service.ts
+++ src/app/map/services/battlefield.service.ts
@@ -16,6 +16,7 @@ export class MapBattlefield {
 
   addCreature(creature: Creature) {
     const existing = this.creatures.find(c => c.location.x === creature.location.x && c.location.y === creature.location.y);
+    // TODO: Figure out if size overlaps too.
     if (existing) { return; }
     this.store.dispatch(new Battlefield.AddCreature(creature));
   }
