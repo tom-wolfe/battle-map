ded0cfb9f6697737c05175b5482417cc8a093ab5
Tom Wolfe <trwolfe13@gmail.com>
2021-01-08 19:17:16 +0000

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@bm/store';
 
+import * as Components from '@bm/navigation';
+import { MapModule } from '@bm/map';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Components.HeaderComponent,
+    Components.MenuComponent
   ],
   imports: [
     BrowserModule,
-    StoreModule
+    StoreModule,
+    MapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/map/map.module.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+// import { RendererModule } from '@bm/renderer';
+// import { ToolboxModule } from '@bm/toolbox';
+// import { EffectsModule } from '@ngrx/effects';
+// import { StoreModule } from '@ngrx/store';
+
+import { MapComponent } from './component/map.component';
+// import * as Services from './services';
+// import { effects } from './store/effects';
+// import { featureName, reducers } from './store/reducer';
+
+const COMPONENTS = [
+  MapComponent
+];
+
+// const SERVICES = [
+//   Services.MapBattlefield,
+//   Services.MapBinding,
+//   Services.MapCanvas,
+//   Services.MapController,
+//   Services.MapGrid,
+//   Services.MapNavigator,
+//   Services.MapTokens
+// ];
+
+@NgModule({
+  declarations: [...COMPONENTS],
+  imports: [
+    CommonModule,
+    // ToolboxModule,
+    // RendererModule,
+    // StoreModule.forFeature(featureName, reducers),
+    // EffectsModule.forFeature(effects),
+  ],
+  exports: [...COMPONENTS],
+  providers: [/*...SERVICES*/],
+  entryComponents: [...COMPONENTS]
+})
+export class MapModule { }
